0f322d49f8c7efa1ef5077718fc647a8ce4a9540
[Ivan Prybolovetz] <iprybolovetz@gmail.com>
2020-03-30 22:30:28 +0300

    Initial Commit

---
+++ Task 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task 1
+//
+//  Created by Ivan Prybolovetz on 3/30/20.
+//  Copyright © 2020 Ivan Prybolovetz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
